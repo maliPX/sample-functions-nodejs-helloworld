f2bdbda4015f1d50e2d11622bbc91ecea6c091fe
Denis M <mali.ms@hotmail.de>
2023-07-20 09:04:20 +0200

    update

--- packages/sample/hello/checkAppOwnership/checkAppOwnership.js
+++
@@ -1,8 +0,0 @@
-function main(args) {
-    let name = args.name || 'stranger'
-    let greeting = 'Hello ' + name + '!'
-    console.log(greeting)
-    return {"body sec": greeting}
-}
-
-exports.main = main;
